0d45fe101d87fcd4a94feb575ed0114a2d92d236
ajar98 <ajay.n.raj@gmail.com>
2013-12-25 09:59:01 -0800

    Create Mho.java

---
+++ Mho.java
@@ -0,0 +1,13 @@
+
+public class Mho extends Actor {
+
+	public Mho(Location loc) {
+		super(loc);
+	}
+	
+	// Location[] places
+	// Location[] possiblePlaces
+	// Location[] possibleLocs
+	// public void nextMove()
+	
+}
