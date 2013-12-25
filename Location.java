6fc1bf2eaddcabd83a2dc6cb1a3caf7b3454e9a8
ajar98 <ajay.n.raj@gmail.com>
2013-12-25 09:27:32 -0800

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+
+public class Location {
+	
+	private final int COL;
+	private final int ROW;
+	
+	public Location(int col, int row) {
+		COL = col;
+		ROW = row;
+	}
+	
+	public int getRow() {
+		return ROW;
+	}
+	
+	public int getCol() {
+		return COL;
+	}
+	
+	public String printLoc() {
+		return "(" + getCol() + ", " + getRow() + ")";
+	}
+	
+ }
+
