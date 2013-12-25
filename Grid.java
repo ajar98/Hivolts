11fe70f11fa2137bd13230f0b5095744e5940301
ajar98 <ajay.n.raj@gmail.com>
2013-12-25 14:42:51 -0800

    Create Grid.java
    Use putActor and isValid to do Fence.java

---
+++ Grid.java
@@ -0,0 +1,30 @@
+
+public class Grid {
+	
+	boolean[][] actors = new boolean[12][12];
+	
+	public Grid() {
+		initActorArray();
+	}
+	
+	void initActorArray() {
+		for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                    actors[i][j] = false;
+            }
+		}
+	}
+	
+	public boolean isValid(Location loc) {
+		boolean validity;
+		if (actors[loc.getCol()][loc.getRow()])
+			validity = true;
+		else
+			validity = false;
+		return validity;
+	}
+	
+	public void putActor(Location loc) {
+		actors[loc.getCol()][loc.getRow()] = true;
+	}
+}
