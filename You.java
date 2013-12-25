79e88baee169f5398025da5e907187ea3654ffb6
ajar98 <ajay.n.raj@gmail.com>
2013-12-25 09:59:38 -0800

    Update You.java

--- You.java
+++ You.java
@@ -1,41 +1,8 @@
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.net.URL;
 
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
-
-// create Fence.java, Mho.java
-
-
-public class You {
+public class You extends Actor {
 	
-	private int youCol;
-	private int youRow;
-	
-	public You (Location loc) {
-		youCol = loc.getCol();
-		youRow = loc.getRow();
-	}
-	
-	public void setCol(int col) {
-		youCol = col;
-	}
-	
-	public void setRow(int row) {
-		youRow = row;
-	}
-	
-	public Location getYouLoc() {
-		return new Location(youCol, youRow);
-	}
-	
-	
-	
-	public void move(Location nextLoc) {
-		setCol(nextLoc.getCol());
-		setRow(nextLoc.getRow());
+	public You(Location loc) {
+		super(loc);
 	}
 	
 	// public void jump()
