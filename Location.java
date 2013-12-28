b3e3c2447d9478b96745a8494f929d342aa6ace1
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:35:10 -0800

    Update Location.java

--- Location.java
+++ Location.java
@@ -2,26 +2,25 @@ import javax.swing.JOptionPane;
 
 
 public class Location {
-	
-	private final int COL;
-	private final int ROW;
-	
-	public Location(int col, int row) {
-		COL = col;
-		ROW = row;
-	}
-	
-	public int getRow() {
-		return ROW;
-	}
-	
-	public int getCol() {
-		return COL;
-	}
-	
-	public String printLoc() {
-		return "(" + getCol() + ", " + getRow() + ")";
-	}
-	
+        
+        private final int COL;
+        private final int ROW;
+        
+        public Location(int col, int row) {
+                COL = col;
+                ROW = row;
+        }
+        
+        public int getRow() {
+                return ROW;
+        }
+        
+        public int getCol() {
+                return COL;
+        }
+        
+        public String printLoc() {
+                return "(" + getCol() + ", " + getRow() + ")";
+        }
+        
  }
-
