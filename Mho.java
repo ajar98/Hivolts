ee49021286181eb8b328b37442c331de1ec293ce
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:35:32 -0800

    Update Mho.java

--- Mho.java
+++ Mho.java
@@ -1,10 +1,32 @@
-
 public class Mho extends Actor {
-
-	public Mho(Location loc, Grid gr) {
-		super(loc, gr, "Mho");
-	}
+	Location youLoc = getGrid().getYou();
+	int row = youLoc.getRow();
+	int col = youLoc.getCol();
+	Location loc;
 	
-	// public void nextMove()
-	
+        public Mho(Location loc, Grid gr) {
+                super(loc, gr, "Mho");
+        }
+        
+        public void nextMove(){
+        	
+        	if (row == loc.getRow()){
+        		if (col < loc.getCol()){
+        			//move to (col + 1, row)
+        		}
+        		else{
+        			//move to (col - 1, row)
+        		}
+        	}
+        	if (col == loc.getCol()){
+        		if (row < loc.getRow()){
+        			//move to (col, row + 1)
+        		}
+        		else{
+        			//move to (col, row - 1)
+        		}
+        	}
+        	
+        }
+        
 }
