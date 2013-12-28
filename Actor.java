d22e6ee578b90670b72486376e96ae481b1e7736
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:33:08 -0800

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -1,53 +1,52 @@
-
 public class Actor {
-	
-	private final Location actorLoc;
-	private int actorCol;
-	private int actorRow;
-	private final Grid g;
-	public String actorName;
-	
-	public Actor(Location loc, Grid gr, String name) { 
-		actorLoc = loc;
-		g = gr;
-		actorName = name;
-		gr.putActor(this);
-	}
-	
-	public String getName() {
-		return actorName;
-	}
-	
-	public void setCol(int col) {
-		actorCol = col;
-	}
-	
-	public boolean checkIfDead() {
-		boolean death;
-		if (g.isValid(actorLoc))
-			death = false;
-		else
-			death = true;
-		return death;
-	}
-	
-	public void setRow(int row) {
-		actorRow = row;
-	}
-	
-	public Location getLoc() {
-		return actorLoc;
-	}
-	
-	
-	
-	public void move(Location nextLoc) {
-		setCol(nextLoc.getCol());
-		setRow(nextLoc.getRow());
-	}
-	
-	public Grid getGrid() {
-		return g;
-	}
-	
+        
+        private final Location actorLoc;
+        private int actorCol;
+        private int actorRow;
+        private final Grid g;
+        public String actorName;
+        
+        public Actor(Location loc, Grid gr, String name) { 
+                actorLoc = loc;
+                g = gr;
+                actorName = name;
+                gr.putActor(this);
+        }
+        
+        public String getName() {
+                return actorName;
+        }
+        
+        public void setCol(int col) {
+                actorCol = col;
+        }
+        
+        public boolean checkIfDead() {
+                boolean death;
+                if (g.isValid(actorLoc))
+                        death = false;
+                else
+                        death = true;
+                return death;
+        }
+        
+        public void setRow(int row) {
+                actorRow = row;
+        }
+        
+        public Location getLoc() {
+                return actorLoc;
+        }
+        
+        
+        
+        public void move(Location nextLoc) {
+                setCol(nextLoc.getCol());
+                setRow(nextLoc.getRow());
+        }
+        
+        public Grid getGrid() {
+                return g;
+        }
+        
 }
