b1648d0252cd020daff978255878dfbcf4e9c719
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:33:59 -0800

    Update You.java

--- You.java
+++ You.java
@@ -3,27 +3,27 @@ import java.util.Random;
 
 
 public class You extends Actor {
-	
-	Grid g = super.getGrid();
-	
-	public You(Location loc, Grid gr) {
-		super(loc, gr, "You");
-	}
-	
-	public void jump() {
-		ArrayList<Location> jumpLocs = new ArrayList<Location>();
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
-				if ((g.getActorNameArray()[i][j].equals("Fence")) || (g.getActorNameArray()[i][j].equals("null"))) {
-					jumpLocs.add(new Location(i, j));
-				}
-			}
-		}
-		Random r = new Random();
-		int jumpIndex = r.nextInt(jumpLocs.size());
-		move(jumpLocs.get(jumpIndex));
-	}
-	
-	// public ArrayList<Location> adjacentLocations()
+        
+        Grid g = super.getGrid();
+        
+        public You(Location loc, Grid gr) {
+                super(loc, gr, "You");
+        }
+        
+        public void jump() {
+                ArrayList<Location> jumpLocs = new ArrayList<Location>();
+                for (int i = 0; i < 12; i++) {
+                        for (int j = 0; j < 12; j++) {
+                                if ((g.getActorNameArray()[i][j].equals("Fence")) || (g.getActorNameArray()[i][j].equals("null"))) {
+                                        jumpLocs.add(new Location(i, j));
+                                }
+                        }
+                }
+                Random r = new Random();
+                int jumpIndex = r.nextInt(jumpLocs.size());
+                move(jumpLocs.get(jumpIndex));
+        }
+        
+        // public ArrayList<Location> adjacentLocations()
 
 }
