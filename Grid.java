c89d90ac87f07d3354a9e162057299a69ae84ac3
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:36:04 -0800

    Update Grid.java

--- Grid.java
+++ Grid.java
@@ -1,50 +1,59 @@
 import java.awt.Graphics;
 
 public class Grid {
-	
-	boolean[][] actors = new boolean[12][12];
-	String[][] actors2 = new String[12][12];
-	
-	public Grid() {
-		initActorArray();
-	}
-	
-	void initActorArray() {
-		for (int i = 0; i < 12; i++) {
+        
+        boolean[][] actors = new boolean[12][12];
+        String[][] actors2 = new String[12][12];
+        
+        public Grid() {
+                initActorArray();
+        }
+        
+        void initActorArray() {
+                for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 12; j++) {
                     actors[i][j] = false;
             }
-		}
-	}
-	
-	void initActorNameArray() {
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
-				actors2[i][j] = "null";
-			}
-		}
-	}
-	
-	public boolean isValid(Location loc) {
-		boolean validity;
-		if (actors[loc.getCol()][loc.getRow()])
-			validity = true;
-		else
-			validity = false;
-		return validity;
-	}
-	
-	public void putActor(Actor a) {
-		actors[a.getLoc().getCol()][a.getLoc().getRow()] = true;
-		actors2[a.getLoc().getCol()][a.getLoc().getRow()] = a.getName();
-	}
-	
-	public boolean[][] getActorArray() {
-		return actors;
-	}
-	
-	public String[][] getActorNameArray() {
-		return actors2;
-	}
-	
+                }
+        }
+        
+        void initActorNameArray() {
+                for (int i = 0; i < 12; i++) {
+                        for (int j = 0; j < 12; j++) {
+                                actors2[i][j] = "null";
+                        }
+                }
+        }
+        
+        public boolean isValid(Location loc) {
+                boolean validity;
+                if (actors[loc.getCol()][loc.getRow()])
+                        validity = true;
+                else
+                        validity = false;
+                return validity;
+        }
+        
+        public void putActor(Actor a) {
+                actors[a.getLoc().getCol()][a.getLoc().getRow()] = true;
+                actors2[a.getLoc().getCol()][a.getLoc().getRow()] = a.getName();
+        }
+        
+        public boolean[][] getActorArray() {
+                return actors;
+        }
+        
+        public String[][] getActorNameArray() {
+                return actors2;
+        }
+        public Location getYou() {
+    		Location youLoc = new Location (-1, -1);
+    		for (int i = 0; i < 12; i++) {
+    			for (int j = 0; j < 12; j++) {
+    				if (actors2[i][j].equalsIgnoreCase("You"))
+    					youLoc = new Location(i, j);
+    			}
+    		}
+    		return youLoc;
+    	}  
 } 
