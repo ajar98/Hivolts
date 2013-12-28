74b9be86676cd4b077f3ff1f8449da8b1c539cca
Insfrt <simon.rufer@yahoo.com>
2013-12-27 17:34:50 -0800

    Update Hivolts.java

--- Hivolts.java
+++ Hivolts.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -25,10 +26,10 @@ public class Hivolts extends JApplet {
         public Hivolts() { }
         
         public void init() {
-        	size = JOptionPane.showInputDialog(null, "Would you like the window size: big, medium or small");
-        	if (size.equalsIgnoreCase("big")) setSize(600, 660);
-        	else if (size.equalsIgnoreCase("medium")) setSize(500, 550);
-        	else if (size.equalsIgnoreCase("small")) setSize(400, 440);
+                size = JOptionPane.showInputDialog(null, "Would you like the window size: big, medium or small");
+                if (size.equalsIgnoreCase("big")) setSize(600, 660);
+                else if (size.equalsIgnoreCase("medium")) setSize(500, 550);
+                else if (size.equalsIgnoreCase("small")) setSize(400, 440);
         }
         
         public void paint(Graphics g) {
@@ -48,8 +49,8 @@ public class Hivolts extends JApplet {
         }
         
         public void playHivolts(int width, int height, Graphics g) {
-        	antiAlias(g);
-        	drawGrid(width, height, g);
+                antiAlias(g);
+                drawGrid(width, height, g);
             placeFences(gr, g);
             placeMhos(gr, g);
             placeYou(gr, g);
@@ -59,90 +60,90 @@ public class Hivolts extends JApplet {
         }
         
         public void nullLoc(Location loc, Graphics g) {
-        	g.setColor(Color.black);
-        	g.fillRect(pixelLoc(loc).getCol() + 1, pixelLoc(loc).getRow() + 1, getCellDim() - 1, getCellDim() - 1);
+                g.setColor(Color.black);
+                g.fillRect(pixelLoc(loc).getCol() + 1, pixelLoc(loc).getRow() + 1, getCellDim() - 1, getCellDim() - 1);
         }
         
         public void placeFences(Grid gr, Graphics g) {
-        	for (Location loc : allAround()) {
-        		Fence fence = new Fence(loc, gr);
-        		drawFence(fence, g);
-        	}
-        	for (Location loc : choosePlaces(20, possibleFencePlaces())) {
-        		Fence fence = new Fence(loc, gr);
-        		drawFence(fence, g);
-        	}
+                for (Location loc : allAround()) {
+                        Fence fence = new Fence(loc, gr);
+                        drawFence(fence, g);
+                }
+                for (Location loc : choosePlaces(20, possibleFencePlaces())) {
+                        Fence fence = new Fence(loc, gr);
+                        drawFence(fence, g);
+                }
         }
         
         public void placeMhos(Grid gr, Graphics g) {
-        	for (Location loc : choosePlaces(12, possibleMhoPlaces(gr))) {
-        		Mho mho = new Mho(loc, gr);
-        		drawMho(mho, g);
-        	}
+                for (Location loc : choosePlaces(12, possibleMhoPlaces(gr))) {
+                        Mho mho = new Mho(loc, gr);
+                        drawMho(mho, g);
+                }
         }
         
         public void placeYou(Grid gr, Graphics g) {
-        	Random r = new Random();
-        	int randLocIndex = r.nextInt(possibleYouPlaces(gr).size());
-        	Location youLoc = possibleYouPlaces(gr).get(randLocIndex);
-        	You you = new You(youLoc, gr);
-        	drawYou(you, g);
+                Random r = new Random();
+                int randLocIndex = r.nextInt(possibleYouPlaces(gr).size());
+                Location youLoc = possibleYouPlaces(gr).get(randLocIndex);
+                You you = new You(youLoc, gr);
+                drawYou(you, g);
         }
         
         public ArrayList<Location> allAround() {
-    		ArrayList<Location> allAround = new ArrayList<Location>();
-    		for (int i = 0; i < 12; i++) {
-    			allAround.add(new Location(i, 0));
-    			allAround.add(new Location(i, 11));
-    			allAround.add(new Location(0, i));
-    			allAround.add(new Location(11, i));
-    		}
-    		return allAround;
-    	}
-    	
-    	public ArrayList<Location> possibleFencePlaces() {
-    		ArrayList<Location> possibleFencePlaces = new ArrayList<Location>();
-    		for (int i = 1; i < 11; i++) {
-    			for (int j = 1; j < 11; j++) {
-    				possibleFencePlaces.add(new Location(i, j));
-    			}
-    		}
-    		Collections.shuffle(possibleFencePlaces);
-    		return possibleFencePlaces;
-    	}
-    	
+                    ArrayList<Location> allAround = new ArrayList<Location>();
+                    for (int i = 0; i < 12; i++) {
+                            allAround.add(new Location(i, 0));
+                            allAround.add(new Location(i, 11));
+                            allAround.add(new Location(0, i));
+                            allAround.add(new Location(11, i));
+                    }
+                    return allAround;
+            }
+            
+            public ArrayList<Location> possibleFencePlaces() {
+                    ArrayList<Location> possibleFencePlaces = new ArrayList<Location>();
+                    for (int i = 1; i < 11; i++) {
+                            for (int j = 1; j < 11; j++) {
+                                    possibleFencePlaces.add(new Location(i, j));
+                            }
+                    }
+                    Collections.shuffle(possibleFencePlaces);
+                    return possibleFencePlaces;
+            }
+            
         public ArrayList<Location> possibleMhoPlaces(Grid gr) {
-        	ArrayList<Location> possibleMhoPlaces = new ArrayList<Location>();
-        	for (Location loc : possibleFencePlaces()) {
-        		if (!gr.isValid(loc)) {
-        			possibleMhoPlaces.add(loc);
-        		}
-        	}
-        	Collections.shuffle(possibleMhoPlaces);
-    		return possibleMhoPlaces;
+                ArrayList<Location> possibleMhoPlaces = new ArrayList<Location>();
+                for (Location loc : possibleFencePlaces()) {
+                        if (!gr.isValid(loc)) {
+                                possibleMhoPlaces.add(loc);
+                        }
+                }
+                Collections.shuffle(possibleMhoPlaces);
+                    return possibleMhoPlaces;
         }
         
         public ArrayList<Location> possibleYouPlaces(Grid gr) {
-        	ArrayList<Location> possibleYouPlaces = new ArrayList<Location>();
-        	for (Location loc : possibleFencePlaces()) {
-        		if (!gr.isValid(loc)) {
-        			possibleYouPlaces.add(loc);
-        		}
-        	}
-        	return possibleYouPlaces;
+                ArrayList<Location> possibleYouPlaces = new ArrayList<Location>();
+                for (Location loc : possibleFencePlaces()) {
+                        if (!gr.isValid(loc)) {
+                                possibleYouPlaces.add(loc);
+                        }
+                }
+                return possibleYouPlaces;
         }
 
         public ArrayList<Location> choosePlaces(int num, ArrayList<Location> possibleLocs) {
-    		ArrayList<Location> places = new ArrayList<Location>();
-    		for (int i = 0; i < num; i++) {
-        		places.add(possibleLocs.get(i));	
-        	}
-    		return places;
-    	}
+                    ArrayList<Location> places = new ArrayList<Location>();
+                    for (int i = 0; i < num; i++) {
+                        places.add(possibleLocs.get(i));        
+                }
+                    return places;
+            }
         
         public void drawYou(You you, Graphics g) {
-        	if (!you.checkIfDead()) {
-        		int width = getCellDim() - 1;
+                if (!you.checkIfDead()) {
+                        int width = getCellDim() - 1;
                 int height = getCellDim() - 1;
                 BufferedImage image = null;
                 try
@@ -155,46 +156,46 @@ public class Hivolts extends JApplet {
                         JOptionPane.showMessageDialog(null, "You.jpg not working because " + e);
                 }
                 g.drawImage(image, pixelLoc(you.getLoc()).getCol() + 1, pixelLoc(you.getLoc()).getRow() + 1, width, height, null);
-        	} else {
-        		finished = false;
-        	}
+                } else {
+                        finished = false;
+                }
                 
         }
         
         public void drawFence(Fence fence, Graphics g){
-        	int width = getCellDim() -1;
-        	int height = getCellDim() -1;
-        	BufferedImage image = null;
-        	try
-        	{
-        		URL u = new URL(getCodeBase(), "Fence.jpg");
-        		image = ImageIO.read(u);
+                int width = getCellDim() -1;
+                int height = getCellDim() -1;
+                BufferedImage image = null;
+                try
+                {
+                        URL u = new URL(getCodeBase(), "Fence.jpg");
+                        image = ImageIO.read(u);
         }
-        	catch(Exception e)
-        	{
-        		JOptionPane.showMessageDialog(null, "Fence.jpg not working because " + e);
-        	}
-        	
-        	g.drawImage(image, pixelLoc(fence.getLoc()).getCol() + 1, pixelLoc(fence.getLoc()).getRow() + 1, width, height, null);
-        	
+                catch(Exception e)
+                {
+                        JOptionPane.showMessageDialog(null, "Fence.jpg not working because " + e);
+                }
+                
+                g.drawImage(image, pixelLoc(fence.getLoc()).getCol() + 1, pixelLoc(fence.getLoc()).getRow() + 1, width, height, null);
+                
         }
         
         public void drawMho(Mho mho, Graphics g){
-        	int width = getCellDim() -1;
-        	int height = getCellDim() -1;
-        	BufferedImage image = null;
-        	try
-        	{
-        		URL u = new URL(getCodeBase(), "Mho.jpg");
-        		image = ImageIO.read(u);
+                int width = getCellDim() -1;
+                int height = getCellDim() -1;
+                BufferedImage image = null;
+                try
+                {
+                        URL u = new URL(getCodeBase(), "Mho.jpg");
+                        image = ImageIO.read(u);
         }
-        	catch(Exception e)
-        	{
-        		JOptionPane.showMessageDialog(null, "Mho.jpg not working because " + e);
-        	}
-        	
-        	g.drawImage(image, pixelLoc(mho.getLoc()).getCol() + 1, pixelLoc(mho.getLoc()).getRow() + 1, width, height, null);
-        	
+                catch(Exception e)
+                {
+                        JOptionPane.showMessageDialog(null, "Mho.jpg not working because " + e);
+                }
+                
+                g.drawImage(image, pixelLoc(mho.getLoc()).getCol() + 1, pixelLoc(mho.getLoc()).getRow() + 1, width, height, null);
+                
         }
         
         public int getCellDim() {
@@ -240,10 +241,10 @@ public class Hivolts extends JApplet {
         }
         
         public void antiAlias(Graphics g) {
-        	((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, // anti aliasing
+                ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, // anti aliasing
                     RenderingHints.VALUE_ANTIALIAS_ON);
         }          
 
 }
         
-        
+ 
