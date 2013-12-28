82f553762da2e5a607ec7e2f51d665d7917ced5d
Insfrt <simon.rufer@yahoo.com>
2013-12-27 18:15:35 -0800

    Update Mho.java

--- Mho.java
+++ Mho.java
@@ -1,3 +1,4 @@
+
 public class Mho extends Actor {
 	Location youLoc = getGrid().getYou();
 	int row = youLoc.getRow();
@@ -10,6 +11,7 @@ public class Mho extends Actor {
         
         public void nextMove(){
         	
+        	// in the same row
         	if (row == loc.getRow()){
         		if (col < loc.getCol()){
         			//move to (col + 1, row)
@@ -18,6 +20,8 @@ public class Mho extends Actor {
         			//move to (col - 1, row)
         		}
         	}
+        	
+        	//in the same col
         	if (col == loc.getCol()){
         		if (row < loc.getRow()){
         			//move to (col, row + 1)
@@ -27,6 +31,44 @@ public class Mho extends Actor {
         		}
         	}
         	
+        	//in the same diagonal
+        	if (Math.abs(row - loc.getRow()) == Math.abs(col - loc.getCol())){
+        		if((loc.getRow() < row) && (loc.getCol() < col)){
+        			//move (col +, row +)
+        		}
+        		if((loc.getRow() > row) && (loc.getCol() < col)){
+        			//move (col +, row -)
+        		}
+        		if ((loc.getRow() < row) && (loc.getCol() > col)){
+        			//move (col -, row +)
+        		}
+        		if ((loc.getRow() > row) && (loc.getCol() > col)){
+        			// move (col -, row -)
+        		}
+        		
+        	}
+        	
+        	//horizontal is greater than vertical
+        	if (Math.abs(col - loc.getCol()) > Math.abs(row - loc.getRow())){
+        		if(loc.getCol() > col){
+        			//move (col +, row)
+        		}
+        		if(loc.getCol() < col){
+        			//move (col -, row )
+        	}
+        	
+        	//vertical is greater than horizontal
+        	if (Math.abs(col - loc.getCol()) < Math.abs(row - loc.getRow())){
+        		if(loc.getRow() > row){
+        			//move (col -, row)
+        		}
+        		if(loc.getRow() < row){
+        			//move (col +, row)
+        		}
+        		
+        	}
+        	
         }
         
+}
 }
