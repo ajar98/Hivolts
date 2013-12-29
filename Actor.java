053becdc49c0420965bce4a32ceb2d9876b883fd
ajar98 <ajay.n.raj@gmail.com>
2013-12-29 10:57:12 -0800

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -1,56 +1,50 @@
 public class Actor {
-        
-        private Location actorLoc;
-        private int actorCol;
-        private int actorRow;
-        private final Grid g;
-        public String actorName;
-        
-        public Actor(Location loc, Grid gr, String name) { 
-             actorLoc = loc;
-             g = gr;
-             actorName = name;
-             gr.putActor(this);
-        }
-        
-        public String getName() {
-                return actorName;
-        }
-        
-        public void setCol(int col) {
-                actorCol = col;
-        }
-        
-        public boolean checkIfDead() {
-                boolean death;
-                if (g.isValid(actorLoc))
-                        death = false;
-                else
-                        death = true;
-                return death;
-        }
-        
-        public void setRow(int row) {
-                actorRow = row;
-        }
-        
-        public void setLoc(Location loc) {
-        	actorLoc = loc;
-        }
-        
-        public Location getLoc() {
-                return actorLoc;
-        }
-        
-        
-        
-        public void move(Location nextLoc) {
-        	g.nullLoc(actorLoc);
-            setLoc(nextLoc);
-        }
-        
-        public Grid getGrid() {
-                return g;
-        }
-        
+
+	private Location actorLoc;
+	private int actorCol;
+	private int actorRow;
+	private final Grid g;
+	public String actorName;
+
+	public Actor(Location loc, Grid gr, String name) { 
+		actorLoc = loc;
+		g = gr;
+		actorName = name;
+		gr.putActor(this);
+	}
+
+	public String getName() {
+		return actorName;
+	}
+
+	public void setCol(int col) {
+		actorCol = col;
+	}
+
+	public void setRow(int row) {
+		actorRow = row;
+	}
+
+	public void setLoc(Location loc) {
+		actorLoc = loc;
+	}
+
+	public Location getLoc() {
+		return actorLoc;
+	}
+
+
+
+	public void move(Location nextLoc) {
+		g.nullLoc(actorLoc);
+		if (getName().equalsIgnoreCase("you")) {
+			setLoc(nextLoc);
+			You you = new You(nextLoc, getGrid());
+		}
+	}
+
+	public Grid getGrid() {
+		return g;
+	}
+
 }
