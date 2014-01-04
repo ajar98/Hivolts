b46f318e5375693997e07bb9db2440e71f5562f9
ajar98 <ajay.n.raj@gmail.com>
2014-01-03 21:18:32 -0800

    Update Fence.java

--- Fence.java
+++ Fence.java
@@ -1,9 +1,3 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
-
 public class Fence extends Actor {	
 
 	public Fence(Location loc, Grid gr) {
