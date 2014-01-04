0607926a4686378e836f6eff81a3b1fa02f81faa
ajar98 <ajay.n.raj@gmail.com>
2014-01-03 19:06:37 -0800

    Update Fence.java

--- Fence.java
+++ Fence.java
@@ -8,25 +8,6 @@ public class Fence extends Actor {
 
 	public Fence(Location loc, Grid gr) {
 		super(loc, gr, "Fence");
-		drawFence();
-	}
-
-	public void drawFence(){
-		int width = getGrid().getCellDim() -1;
-		int height = getGrid().getCellDim() -1;
-		BufferedImage image = null;
-		try
-		{
-			image = ImageIO.read(new File("Fence.jpg"));
-		}
-		catch(Exception e)
-		{
-			JOptionPane.showMessageDialog(null, "Fence.jpg not working because " + e);
-		}
-
-		getGrid().getGraphics().drawImage(image, getGrid().pixelLoc(getLoc()).getCol() + 1, getGrid().pixelLoc(getLoc()).getRow() + 1, width, height, null);
-
-
 	}
 
 }
